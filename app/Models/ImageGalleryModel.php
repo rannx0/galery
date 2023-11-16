205850e7dd32b2325a378225edcdd6ad08549fbd
Ranu <ranuslebew19@gmail.com>
2023-11-16 22:36:14 +0700

    route jeng Models

--- app/Models/ImageGalleryModel.php
+++ app/Models/ImageGalleryModel.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ImageGalleryModel extends Model
 {
-    use HasFactory;
+    protected $table = 'image_gallery';
+
+
+    protected $fillable = ['title','image'];
 }
